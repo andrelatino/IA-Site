0ae51b14efe9b1e3f800fb7fac4e894f6e3be374
andrelatino <73903713+andrelatino@users.noreply.github.com>
2023-10-25 08:22:34 +0200

    Add servers

---
+++ servers/servers-list.js
@@ -0,0 +1,51 @@
+
+// Create a map to associate functions with plugin IDs
+const pluginFunctions = {
+  "unsplash": unsplash_check_api,
+  "github": github_check_api,
+  "pexels": pexels_check_api,
+};
+
+function serversList() {
+  fetch("servers.json")
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => {
+          const getTotal = data.length;
+          const showTotal = getTotal === undefined ? 0 : getTotal;
+
+          const addSubtitle = document.getElementById('grid');
+          addSubtitle.className = 'servers-grid';
+          addSubtitle.innerHTML = `
+              <div class='servers-subtitle'>
+              <p>Total: ${showTotal}</p>
+              <button id="addNewSite" onclick="modalOpen();" class="servers-add">
+                  <img class="addIcon" src="../global/file/add.svg">
+              </button>
+              </div>`;
+
+          const gridDiv = document.getElementById('grid');
+          for (const plugin of data) {
+            const itemsDiv = document.createElement('div');
+            itemsDiv.className = 'servers-items';
+            itemsDiv.innerHTML = `
+                <div class="servers-content">       
+                <img class="admin-sidebar-plugin-icon" src="${plugin.icon}">
+                <h2>${plugin.title}</h2>				
+                <input id="${plugin.id}_input" type="password" required>
+                <button id="${plugin.id}_btn" class="servers-button">Check API Key</button>	
+                </div>`;
+            gridDiv.appendChild(itemsDiv);
+            // Attach the corresponding function to the button's click event
+            document.getElementById(`${plugin.id}_btn`).addEventListener('click', pluginFunctions[plugin.id]);
+        }
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
+}
+serversList();
